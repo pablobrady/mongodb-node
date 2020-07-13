a37b6d2a77356cf795de7fb567bd6bdbd774fa00
Paul Brady <pablo.brady@gmail.com>
2020-07-12 17:45:43 -0700

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const cool = require('cool-acsii-faces');
 const express = require('express');
 // const connectDB = require('./DB/Connection')
 const app = express();
+const PORT = process.env.Port || 3000;
 
 // connectDB();
 
@@ -8,11 +10,13 @@ const app = express();
 
 app.use(express.json({ extended:false }));
 // app.use('/api/userModel', require('./API/User'));
-const PORT = process.env.Port || 3000;
+
 
 
 app.get('/', function (req, res) {
   res.send('Hello World -- Mongo is here!')
 })
 
-app.listen(PORT,() => console.log("Server started"));
+app.get('/cool', (req, res) => res.send(cool()));
+
+app.listen(PORT, () => console.log("Server started"));
